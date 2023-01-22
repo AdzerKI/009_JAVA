29215129026a39d2b6384cf374a69960543536b0
AdzerKI <silver_cat@list.ru>
2023-01-23 00:24:50 +0300

    lection one final

--- lectionOne/src/ExTwo.java
+++ lectionOne/src/ExTwo.java
@@ -1,55 +1,54 @@
 import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.stream.Collectors;
 
 /*
  * Даны 2 файла, в каждом из которых записан многочлен, сформировать файл с суммой многочленов
  */
 
 public class ExTwo {
-    public boolean exTwo() {
-        // Первый многочлен
-        try(FileReader reader = new FileReader("lectionOne\\first.txt"))
+
+    final static String DIR = "C:\\GitHub\\geekbrains\\009_JAVA\\lectionOne\\data\\";
+
+    public static void exTwo() throws FileNotFoundException {
+
+        // Считывааем файл 1
+        String filePathOne = "data/first.txt";
+        String contentOne = null;
+
+        String filePathTwo = "data/second.txt";
+        String contentTwo = null;
+
+        // Считываем файл 1
+        try {
+            contentOne = readFile(filePathOne);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Считываем файл 2
+        try {
+            contentTwo = readFile(filePathTwo);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Записываем в 3 файл
+        try (PrintWriter out = new PrintWriter(DIR + "result.txt", StandardCharsets.UTF_8))
         {
-            // читаем посимвольно
-            int c;
-            while((c=reader.read())!=-1){
-
-                System.out.print((char)c);
-            }
-        }
-        catch(IOException ex){
-
-            System.out.println(ex.getMessage());
-        }
-
-        // Второй многочлен
-        try(FileReader reader = new FileReader("lectionOne\\second.txt"))
-        {
-            // читаем посимвольно
-            int c;
-            while((c=reader.read())!=-1){
-
-                System.out.print((char)c);
-            }
-        }
-        catch(IOException ex){
-
-            System.out.println(ex.getMessage());
-        }
-
-        try(FileWriter writer = new FileWriter("second.txt", false))
-        {
-            // запись всей строки
-            String text = "Hello Gold!";
-            writer.write(text);
-            // запись по символам
-            writer.append('\n');
-            writer.append('E');
-
-            writer.flush();
-        }
-        catch(IOException ex){
-
-            System.out.println(ex.getMessage());
+            out.print(contentOne);
+            out.print("\n");
+            out.print(contentTwo);
+            System.out.println("Successfully written data to the file");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
+
+    public static String readFile(String path) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(new File(path)));
+        return reader.lines().collect(Collectors.joining(System.lineSeparator()));
+    }
+
+
 }
--- lectionOne/src/Main.java
+++ lectionOne/src/Main.java
@@ -1,7 +1,8 @@
+import java.io.FileNotFoundException;
+
 public class Main {
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) throws FileNotFoundException {
         /*
          * Задана натуральная степень k. Сформировать случайным способом список коэффициентов (от 0 до 100)
          * многочлена многочлен степени k
@@ -12,6 +13,7 @@ public class Main {
         /*
          * Даны 2 файла, в каждом из которых записан многочлен, сформировать файл с суммой многочленов
          */
+//        ExTwo.exTwo();
 
     }
 }
